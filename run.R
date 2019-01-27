37057b4fc20ec1c389b556704e56a3949b0779f8
Jonathan Sidi <yonis@metrumrg.com>
2019-01-26 21:16:21 -0500

    update run

--- run.R
+++ run.R
@@ -1,7 +1,10 @@
 library(toddlr)
 
-shiny::runApp(
-  appDir = getwd(),
-  host = '0.0.0.0',
-  port = as.numeric(Sys.getenv('PORT'))
-)
+options(shiny.host = '0.0.0.0')
+options(shiny.port = Sys.getenv('PORT'))
+
+# shiny::runApp(
+#   appDir = getwd(),
+#   host = '0.0.0.0',
+#   port = as.numeric(Sys.getenv('PORT'))
+# )
