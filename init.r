90322a3d0ae2152271fd770bef639ca3021cf4f7
Jonathan Sidi <yonis@metrumrg.com>
2019-02-12 08:18:34 -0500

    add viridis lib

--- init.r
+++ init.r
@@ -23,7 +23,7 @@ install_if_missing_gh = function(p) {
 
 my_packages_gh = c("d3treeR/d3treeR","daattali/timevis")
 
-my_packages = c('ggplot2','slickR','dplyr')
+my_packages = c('ggplot2','slickR','dplyr','viridis')
 
  install.packages(c('Rcpp','remotes'), dependencies = TRUE)
  invisible(sapply(my_packages, install_if_missing))
