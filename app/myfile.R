07538e745ee950b635120f1384b67f96dc0aaf37
Jonathan Sidi <yonis@metrumrg.com>
2019-04-28 11:23:45 -0400

    move app.R to app dir

--- app/myfile.R
+++ app/myfile.R
@@ -6,6 +6,9 @@ normalMean <- function(samples=10){
   mean(data)
 }
 
+#* Return the sum of two numbers
+#* @param a The first number to add
+#* @param b The second number to add
 #* @post /sum
 addTwo <- function(a, b){
   as.numeric(a) + as.numeric(b)
