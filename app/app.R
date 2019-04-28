69b400211e05d7ebfac057954b0ffd4c0a950ff1
Jonathan Sidi <yonis@metrumrg.com>
2019-04-28 13:20:09 -0400

    set port in app.R

--- app/app.R
+++ app/app.R
@@ -4,4 +4,8 @@ port <- Sys.getenv('PORT')
 
 r <- plumber::plumb("app/myfile.R")
 
-r$run(host='0.0.0.0', port=strtoi(port),swagger = TRUE)
+if(Sys.getenv("PORT") == "") Sys.setenv(PORT = 8000)
+
+r$run(host = "0.0.0.0", port=as.numeric(Sys.getenv("PORT")), swagger = TRUE)
+
+# r$run(host='0.0.0.0', port=strtoi(port),swagger = TRUE)
