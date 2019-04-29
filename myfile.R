f43660c2feb37d7235c25d38f8959a8a6142e201
Jonathan Sidi <yonis@metrumrg.com>
2019-04-29 16:40:31 -0400

    add test to myfile

--- myfile.R
+++ myfile.R
@@ -1,18 +1,28 @@
 # myfile.R
 
-#* Random sample
-#* @param samples sample size
-#* @get /mean
-normalMean <- function(samples=10){
-  rnorm(samples)
-}
-
-#* Return the sum of two numbers
-#* @param a The first number to add
-#* @param b The second number to add
-#* @post /sum
-addTwo <- function(a, b){
-  as.numeric(a) + as.numeric(b)
+#* @get /
+#* @html
+function(req, res) {
+'<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="68" height="20">
+  <linearGradient id="b" x2="0" y2="100%">
+    <stop offset="0" stop-color="#bbb" stop-opacity=".1"/>
+    <stop offset="1" stop-opacity=".1"/>
+  </linearGradient>
+  <clipPath id="a">
+    <rect width="68" height="20" rx="3" fill="#fff"/>
+  </clipPath>
+  <g clip-path="url(#a)">
+    <path fill="#555" d="M0 0h45v20H0z"/>
+    <path fill="#f39f37" d="M45 0h23v20H45z"/>
+    <path fill="url(#b)" d="M0 0h68v20H0z"/>
+  </g>
+  <g fill="#fff" text-anchor="middle" font-family="DejaVu Sans,Verdana,Geneva,sans-serif" font-size="110">
+    <text x="235" y="150" fill="#010101" fill-opacity=".3" transform="scale(.1)" textLength="350">clones</text>
+    <text x="235" y="140" transform="scale(.1)" textLength="350">clones</text>
+    <text x="555" y="150" fill="#010101" fill-opacity=".3" transform="scale(.1)" textLength="130">13</text>
+    <text x="555" y="140" transform="scale(.1)" textLength="130">13</text>
+  </g> 
+  </svg>'
 }
 
 #* Github Clones
