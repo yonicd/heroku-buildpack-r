032c727ac436529a9647ac7c9f1a56ca2443779d
Jonathan Sidi <yonis@metrumrg.com>
2019-04-30 09:30:27 -0400

    add heartbeat,docs

--- myfile.R
+++ myfile.R
@@ -32,10 +32,45 @@ views <- function(owner, repo, stat){
 #* @param owner github owner
 #* @param repo github repo
 #* @param type type of data clones or views
-#* @get /repo_data
+#* @get /data
 repo_data <- function(owner,repo,type){
   
   get_data(owner = owner, repo = repo, type = type)
   
 }
 
+#* heartbeat
+#* @html
+#* @get /heartbeat
+heartbeat <- function(){
+  '{
+    "routes": [
+      "/docs (GET)",
+      "/heartbeat (GET)",
+      "/clones (GET)",
+      "/views (GET)",
+      "/data  (GET)"
+      ]
+  }'
+}
+
+#' @get /docs
+#' @html
+function(req, res) {
+  res$status <- 303 # redirect
+  res$setHeader("Location", "https://github.com")
+  "<html>
+  <head>
+    <meta http-equiv=\"Refresh\" content=\"0; url=https://github.com\" />
+  </head>
+  <body>
+    <p>Please follow <a href=\"https://github.com/\">this link</a>.</p>
+  </body>
+</html>"
+}
+
+#' @get /docs2
+#* @html
+function() {
+  markdown::markdownToHTML(text = '![](https://github.com/yonicd/whereami/blob/media/whereami_shiny.gif?raw=true)')
+}
